24afa656318006deefadeeea9fb6a2d5458ec0c3
TroExol <zeidxol@gmail.com>
2022-10-23 19:46:09 +0500

    -require auth on main page

--- src/api/methods.js
+++ src/api/methods.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import applyCaseMiddleware from 'axios-case-converter';
 
 import Format from './intercepters/format';
-import RequireAuth from './intercepters/requireAuth';
 
 const axiosWithConverter = applyCaseMiddleware(axios.create(), {
     ignoreHeaders: true,
@@ -14,13 +13,11 @@ class Methods {
         return axiosWithConverter.post('/api/v1/login/', params);
     }
     
-    @RequireAuth
     @Format
     FetchProfessionList(params) {
         return axiosWithConverter.get('/api/v1/professions/', {params});
     }
     
-    @RequireAuth
     @Format
     FetchAreaList(params) {
         return axiosWithConverter.get('/api/v1/areas/', {params});
