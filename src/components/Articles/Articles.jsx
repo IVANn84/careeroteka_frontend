335ef79be3cb9bf1971eb13d7ee3b93c736df4b8
TroExol <zeidxol@gmail.com>
2022-11-16 14:05:28 +0500

    remove extra article

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,8 +1,7 @@
 import React, {useRef} from 'react';
 import {Link} from 'react-router-dom';
 
-import relocation1Image from 'Image/articles/relocation-1.svg';
-import relocation2Image from 'Image/articles/relocation-2.svg';
+import relocationImage from 'Image/articles/relocation.svg';
 import aboutUsImage from 'Image/articles/about-us.svg';
 import buildCareerPlanImage from 'Image/articles/build-career-plan.svg';
 
@@ -11,18 +10,14 @@ import {InitSlider} from 'Util/slider';
 const articleList = [
     {
         id: 1,
-        image: relocation1Image,
+        image: relocationImage,
     },
     {
         id: 2,
-        image: relocation2Image,
-    },
-    {
-        id: 3,
         image: aboutUsImage,
     },
     {
-        id: 4,
+        id: 3,
         image: buildCareerPlanImage,
     },
 ];
