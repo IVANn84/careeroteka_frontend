9627afda97dafe2e5e41cc57198ae9eca349b55c
TroExol <zeidxol@gmail.com>
2023-04-12 16:46:28 +0500

    CAR-25 +onboarding page +verify email page

---
+++ src/pages/Onboarding/components/blocks/Step/components/Step3/components/SkillList/components/Skill/index.js
@@ -0,0 +1,27 @@
+import withStyle from 'react-jss';
+import {memo} from 'react';
+
+import Skill from './Skill.jsx';
+
+const style = ({font}) => ({
+    container: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        minWidth: 200,
+        borderRadius: 8,
+        padding: ({removeSkill}) => 12 + !!removeSkill,
+        cursor: ({removeSkill}) => !removeSkill && 'pointer',
+        border: ({removeSkill}) => !removeSkill && [[1, 'solid', font.color.regular]],
+        background: ({removeSkill}) => removeSkill && font.color.alternative,
+        color: ({removeSkill}) => removeSkill && font.color.light,
+    },
+    title: {
+        wordBreak: 'break-word',
+    },
+    removeButton: {
+        cursor: 'pointer',
+    },
+});
+
+export default memo(withStyle(style)(Skill));
