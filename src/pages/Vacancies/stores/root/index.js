afcc0fdc57bdc998dbd0c176b2a39e6cd319d7cf
TroExol <zeidxol@gmail.com>
2023-08-11 13:01:34 +0500

    CAR-140 -filters modal

--- src/pages/Vacancies/stores/root/index.js
+++ src/pages/Vacancies/stores/root/index.js
@@ -3,7 +3,6 @@ import { types } from 'mobx-state-tree';
 import { fieldsStoreVacanciesPage, FieldsStoreModel } from '../fields';
 import { vacanciesStoreMainPage, VacanciesStoreModel } from '../vacancies';
 import { gradesStoreVacanciesPage, GradesStoreModel } from '../grades';
-import { rootStoreFiltersModalVacanciesPage, RootStoreFiltersModalModel } from '../FiltersModal/root';
 import actions from './actions';
 
 export const RootStoreModel = types
@@ -11,7 +10,6 @@ export const RootStoreModel = types
     fieldsStore: types.maybe(FieldsStoreModel),
     vacanciesStore: types.maybe(VacanciesStoreModel),
     gradesStore: types.maybe(GradesStoreModel),
-    filtersModalStore: types.maybe(RootStoreFiltersModalModel),
   })
   .actions(actions);
 
@@ -19,5 +17,4 @@ export const rootStoreVacanciesPage = RootStoreModel.create({
   fieldsStore: fieldsStoreVacanciesPage,
   vacanciesStore: vacanciesStoreMainPage,
   gradesStore: gradesStoreVacanciesPage,
-  filtersModalStore: rootStoreFiltersModalVacanciesPage,
 });
