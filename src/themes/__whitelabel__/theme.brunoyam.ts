045dd42a75b6936dcc5d73f3af20c56c9919015c
Aleksandr Dronov <dronovspb@gmail.com>
2024-08-07 20:59:05 +0300

    fix types

--- src/themes/__whitelabel__/theme.brunoyam.ts
+++ src/themes/__whitelabel__/theme.brunoyam.ts
@@ -314,6 +314,10 @@ export const Theme: ThemeType = {
         fontSize: 24,
         lineHeight: '30px',
       },
+      H1Lending: {
+        fontSize: 74,
+        lineHeight: '78px',
+      },
 
       // Body
       B1: {
